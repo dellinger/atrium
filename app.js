842912d4ae3e38a29e74a58c61fae4bb9d23851e
David Ellinger <david.ellinger@me.com>
2014-08-04 18:43:47 -0700

    Created a 'connect' directory structure

--- app.js
+++ app.js
@@ -3,31 +3,22 @@
  * @author David Ellinger
  */
 
- var http = require('http');
- var mongoose = require('mongoose');
 
- var db = mongoose.connect('mongodb://localhost/test');
+ var express = require('express');
+ var app = express();
+ //var mongoose = require('mongoose');
+
+// var db = mongoose.connect('mongodb://localhost/test');
 
  //Register schema
- var Schema = mongoose.Schema;
+/* var Schema = mongoose.Schema;
  var Items = new Schema({
  	name : String,
  	description : String
  });
- mongoose.model('Item', Items);
+ mongoose.model('Item', Items);*/
 
-
-
- var server = http.createServer(function(req, res){
- 	var Item = mongoose.model('Item');
- 	var item = new Item();
- 	item.name = 'test';
- 	item.description = 'I doubt this will work';
- 	item.save(function(err){
- 		if(err) throw err;
- 		console.log("Item saved.");
- 	})
- 	res.end("YAY");
-
- });
- server.listen(3000);
+app.get('/', function(req, res){
+	res.send('Hello');
+});
+app.listen(3000);
