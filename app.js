79516b69cf0b3e63a2d3c9a6aefa71c466481027
David Ellinger <david.ellinger@me.com>
2014-08-04 18:56:40 -0700

    Generated directories by actually using express command. Using ejs templating engine.

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
-/*
- * Main App file App.js
- * @author David Ellinger
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
- var express = require('express');
- var app = express();
- //var mongoose = require('mongoose');
+var app = express();
 
-// var db = mongoose.connect('mongodb://localhost/test');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
- //Register schema
-/* var Schema = mongoose.Schema;
- var Items = new Schema({
- 	name : String,
- 	description : String
- });
- mongoose.model('Item', Items);*/
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-	res.send('Hello');
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-app.listen(3000);
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
