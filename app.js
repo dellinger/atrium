b38a72b6db33c5fd9fc72fb574e93bab2af1bb89
David Ellinger <david.ellinger@me.com>
2014-08-04 18:39:09 -0700

    Setup mongoose and connected to DB. Saving a model successfully

--- app.js
+++ app.js
@@ -4,8 +4,30 @@
  */
 
  var http = require('http');
+ var mongoose = require('mongoose');
+
+ var db = mongoose.connect('mongodb://localhost/test');
+
+ //Register schema
+ var Schema = mongoose.Schema;
+ var Items = new Schema({
+ 	name : String,
+ 	description : String
+ });
+ mongoose.model('Item', Items);
+
+
 
  var server = http.createServer(function(req, res){
+ 	var Item = mongoose.model('Item');
+ 	var item = new Item();
+ 	item.name = 'test';
+ 	item.description = 'I doubt this will work';
+ 	item.save(function(err){
+ 		if(err) throw err;
+ 		console.log("Item saved.");
+ 	})
  	res.end("YAY");
+
  });
  server.listen(3000);
